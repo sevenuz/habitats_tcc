c4e57ab3c74ec431da3e6a6370946cae3ed818d6
sevenuz <julius@athenstaedt.net>
2025-03-27 14:04:16 +0100

    refactor game out of main.lua

--- main.lua
+++ main.lua
@@ -1,10 +1,9 @@
-Card = require("src/card")
-Deck = require("src/deck")
 Stack = require("src/util/stack")
 
--- https://github.com/speakk/lighter
 credits_menu = require("src/menu/credits")
 main_menu = require("src/menu/mainmenu")
+
+game = require("src/game")
 
 VERSION = '0.0.1'
 
@@ -23,48 +22,10 @@ function love.load()
 	credits_menu.load()
 	main_menu.load()
 
-	winned = false
-
-	-- Game timer
-	startTime = 0
-	gameTimer = 0
+	game.load()
 
 	-- Set title
 	love.window.setTitle(TITLE)
-
-	-- Create new deck
-	deck = Deck:new()
-
-	-- Deck params
-	local cardWidth = 90
-	local cardHeight = 120
-	local gridSize = 28
-	local spacing = 15
-
-	-- Fill deck
-	deck:fill(windowWidth, windowHeight, cardWidth, cardHeight, gridSize,
-		spacing)
-
-	-- Suffle added cards
-	deck:shuffle()
-
-	-- Get cards size for own sprites
-	adjustedCardWidth = deck:get(1).width
-	adjustedCardHeight = deck:get(1).height
-
-	-- Load background sprite
-	backgroundQuad = love.graphics.newQuad(0, 0, windowWidth, windowHeight, 400, 400)
-	backgroundImage = love.graphics.newImage('sprites/back.png')
-	backgroundImage:setWrap('repeat', 'repeat')
-
-	-- Load cards sprites
-	cardBackImage = love.graphics.newImage('sprites/cardback.png')
-	cardBackQuad = love.graphics.newQuad(0, 0, adjustedCardWidth,
-		adjustedCardHeight, adjustedCardWidth,
-		adjustedCardHeight)
-	cardFrontImage = love.graphics.newImage('sprites/cardfront.png')
-	cardFrontQuad = love.graphics.newQuad(0, 0, adjustedCardWidth,
-		adjustedCardHeight, adjustedCardWidth, adjustedCardHeight)
 end
 
 function love.draw()
@@ -82,49 +43,10 @@ function love.draw()
 		return
 	end
 
-	-- Check winned
-	if winned then
-		show_winned()
+	if stack:peek() == GAME then
+		game.draw()
 		return
 	end
-
-	-- Iterate over cards
-	for i = 1, deck:count() do
-		local d = deck:get(i)
-
-		love.graphics.setColor(255, 255, 255, 255)
-
-		if not d.isSolved then
-			-- Draw card
-			if d.isOpened then
-				love.graphics.draw(cardFrontImage, cardFrontQuad, d.x, d.y)
-
-				-- Params for card text
-				local limit = 60
-				local x = (d.x + d.width / 2) - limit / 2
-				local y = (d.y + d.height / 2) - limit / 4
-				local cardText = (d.isOpened and d.name or "#")
-
-				-- Draw card text
-				love.graphics.setFont(love.graphics.newFont(limit / 2))
-				love.graphics.setColor(255, 0, 0)
-				love.graphics.printf(cardText, x, y, limit, 'center')
-			else
-				love.graphics.draw(cardBackImage, cardBackQuad, d.x, d.y)
-			end
-		end
-	end
-
-	-- Draw version
-	love.graphics.setFont(love.graphics.newFont(11))
-	love.graphics.setColor(255, 255, 255)
-	love.graphics.print('v' .. VERSION, windowWidth - 40, windowHeight - 15)
-
-	-- Draw game timer
-	if startTime > 0 and not winned then
-		gameTimer = love.timer.getTime() - startTime
-	end
-	love.graphics.print(string.format('%.2f', gameTimer), 0, 0)
 end
 
 function love.update(dt)
@@ -134,49 +56,9 @@ function love.update(dt)
 	if stack:peek() == MAIN_MENU then
 		main_menu.update(dt)
 	end
-
-	-- Check count of openned cards
-	if deck:solvedCount() == deck:count() then
-		winned = true
-		return
+	if stack:peek() == GAME then
+		game.update(dt)
 	end
-
-	local firstCard = nil
-	local secondCard = nil
-
-	-- Search opened cards
-	for i = 1, deck:count() do
-		local c = deck:get(i)
-
-		if c.isOpened and not c.isSolved and firstCard == nil then
-			firstCard = c
-			goto continue
-		end
-
-		if c.isOpened and not c.isSolved and secondCard == nil then
-			secondCard = c
-		end
-		::continue::
-	end
-
-	-- Update card status
-	if firstCard ~= nil and secondCard ~= nil then
-		if (firstCard.name == secondCard.name) then
-			firstCard.isSolved = true
-			secondCard.isSolved = true
-		else
-			firstCard:autoclose(love.timer.getTime())
-			secondCard:autoclose(love.timer.getTime())
-		end
-
-		return
-	end
-
-	-- Autoclose
-	if firstCard ~= nil then firstCard:autoclose(love.timer.getTime()) end
-
-	-- Autoclose
-	if secondCard ~= nil then secondCard:autoclose(love.timer.getTime()) end
 end
 
 function love.mousepressed(x, y, button, istouch, presses)
@@ -188,28 +70,7 @@ function love.mousepressed(x, y, button, istouch, presses)
 	end
 
 	if stack:peek() == GAME then
-		-- Left button pressed
-		if button == 1 then
-			if deck:openedCount() >= 2 then
-				deck:closeAll()
-			else
-				-- Iterate over cards
-				for i = 1, deck:count() do
-					local c = deck:get(i)
-
-					-- Search card by coords
-					if not c.isSolved and c:inside(x, y) then
-						c.isOpened = true
-						c.openedTime = love.timer.getTime()
-
-						-- First set game timer
-						if startTime == 0 then
-							startTime = love.timer.getTime()
-						end
-					end
-				end
-			end
-		end
+		game.mousepressed(x, y, button, istouch, presses)
 	end
 end
 
@@ -264,12 +125,4 @@ function love.quit()
 	print("Thanks for playing! Come back soon!")
 	-- Clean up
 	main_menu.quit()
-end
-
--- Show winned screen
-function show_winned()
-	love.graphics.setFont(love.graphics.newFont(60))
-	love.graphics.setColor(255, 255, 255)
-	love.graphics.printf(string.format('You Win \nin\n%.2f sec!', gameTimer),
-		windowWidth / 5.5, windowHeight / 3.5, 500, 'center')
 end
